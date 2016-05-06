ca607b8bfb3057e79eae9eb50b3244447cd6dc1f
max <maxshuhaliia@gmail.com>
2016-05-06 22:57:34 +0300

    hire me nodemailer

--- handlers/hireHandler.js
+++ handlers/hireHandler.js
@@ -1,3 +1,4 @@
+var nodemailer = require('nodemailer');
 
 function sendMessageOnPhone(BodyMessage, res){
 
@@ -5,6 +6,7 @@ function sendMessageOnPhone(BodyMessage, res){
     var authToken = '45f87fe77ddd4aef3feebd070da5434e';
 
     var client = require('twilio')(accountSid, authToken);
+
     client.messages.create({
         to: "+380631038657",
         from: "+12017629436",
@@ -12,14 +14,42 @@ function sendMessageOnPhone(BodyMessage, res){
     }, function(err, message) {
 
         if(err){
-          return res.status(500).send(err);
+            console.log(err);
+          //return res.status(500).send(err);
         }else{
-           return res.status(200).send({messageStatus: 'Was delivered'});
+          // return res.status(200).send({messageStatus: 'Was delivered'});
         }
     });
 };
 
-function sendMessageOnMail(BodyMessage, res){
+function sendMessageOnMail(BodyMessage, replyEmail, res){
+
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'maxshuhaliia@gmail.com', // Your email id
+            pass: 'unrealman111111' // Your password
+        }
+    });
+
+    var mailOptions = {
+        from: replyEmail,
+        to: 'maxshuhaliia@gmail.com', // list of receivers
+        subject: 'Email Example', // Subject line
+        text: BodyMessage
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);
+            res.json({yo: 'error'});
+        }else{
+            console.log('Message sent: ' + info.response);
+            res.json({yo: info.response});
+        };
+    });
+
+
 
 };
 
@@ -31,8 +61,9 @@ module.exports = function() {
         var companyName = body.companyName;
         var replyEmail = body.replyEmail;
         var message = body.message;
-        var messageToMe = name + " from company '" + companyName + "'with contacts '"  + replyEmail + "' wanna hire me!!!" +
+        var messageToMe = name + " from company '" + companyName + "' with contacts '"  + replyEmail + "' wanna hire me!!!" +
                 "his message: '" + message + "'";
-       // sendMessageOnPhone(messageToMe, res);
+        sendMessageOnPhone(messageToMe, res);
+        sendMessageOnMail(messageToMe, replyEmail, res);
     }
 };
